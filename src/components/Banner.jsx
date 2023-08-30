cafaff853d01f509e7338983b274bd3c54300faf
Giancarlo <giancarlo.scalisi@dci.education>
2023-08-30 17:48:03 +0200

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,20 +13,17 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section
-      className="min-h-[85vh] lg:min-h[78vh] flex items-center"
-      id="home"
-    >
-      <div className="container mx-auto">
-        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+    <section className="section" id="home">
+      <div className="container">
+        <div className="container-flex">
           {/* text */}
-          <div className="flex-1 text-center font-secondary lg:text-left">
+          <div className="text">
             <motion.h1
               variants={fadeIn("up", 0.3)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+              className="motion-h1"
             >
               Zcalacee
               {/* <span></span> */}
@@ -36,10 +33,11 @@ const Banner = () => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="mb-6 text-[36px] lg:text-[60px] font-secondary
-            font-semibold uppercase leading-[1]"
+              className="motion-div"
             >
-              <span className="text-white mr-4">I am a</span>
+              <div>
+                <span className="text-white">I am a</span>
+              </div>
               <TypeAnimation
                 sequence={[
                   "Singer",
@@ -61,7 +59,7 @@ const Banner = () => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="flex text-[20px] gap-x-6 max-w-max mx-auto mb-5 lg:gx-0"
+              className="motion-div-children"
             >
               <a href="#">
                 <FaSpotify className="spotify" />
@@ -79,9 +77,9 @@ const Banner = () => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+              className="motion-div-children-2"
             >
-              <button className="btn btn-lg">Contact me</button>
+              <button className="btn">Contact me</button>
               <a href="#" className="text-gradient btn-link">
                 My Website
               </a>
@@ -93,7 +91,7 @@ const Banner = () => {
             variants={fadeIn("down", 0.5)}
             initial="hidden"
             whileInView={"show"}
-            className=" lg:flex flex-1 max-w-[480px] lg:max-w-[482px]"
+            className="motion-div-element"
           >
             <img src={Image} alt="zcalacee portrait" />
           </motion.div>
--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+//import { motion } from "framer-motion";
+//import { fadeIn } from "../variants";
+import belalim from "../assets/belalim2023.jpg";
+import galerie from "../assets/galerie.jpg";
+import poladior from "../assets/poladior.jpg";
+import "./MusicStyles.css";
 
 const Music = () => {
-  return <div>Music</div>;
+  return (
+    <section className="section" id="music">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-12 mb-10 lg:mb-0">
+          <div>
+            <h2 className="h2 leading-tight text-gradient">Meine Songs</h2>
+            <p className="max-w-sm mb-8 font-bold">
+              Alle meine Songs auf Spotify
+            </p>
+            <button className="btn btn-sm">Hier alle Songs</button>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="group relative overflow-hidden border-2 border-white/90 rounded-xl">
+              <div></div>
+              <img
+                className="imageCover w-full h-84 object-cover"
+                src={belalim}
+                alt="belalim cover"
+              />
+              <div className="text-gradient">Zcalacee feat. Ziya</div>
+              <div>
+                <span className="text-gradient">Belalim 2023</span>
+                <button className="playButton">Play</button>
+              </div>
+              <div flex-1></div>
+            </div>
+
+            <div className="group relative overflow-hidden border-2 border-white/90 rounded-xl">
+              <div></div>
+              <img
+                className="imageCover w-full h-84 object-cover"
+                src={galerie}
+                alt="galerie cover"
+              />
+              <div className="text-gradient">Zcalacee feat. Momo</div>
+              <div>
+                <span className="text-gradient">Galerie</span>
+                <button className="playButton">Play</button>
+              </div>
+              <div flex-1></div>
+            </div>
+
+            <div className="group relative overflow-hidden border-2 border-white/90 rounded-xl">
+              <div></div>
+              <img
+                className="imageCover w-full h-84 object-cover"
+                src={poladior}
+                alt="poladior cover"
+              />
+              <div className="text-gradient">Zcalacee</div>
+              <div>
+                <span className="text-gradient">Poladior</span>
+                <button className="playButton">Play</button>
+              </div>
+              <div flex-1></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Music;
