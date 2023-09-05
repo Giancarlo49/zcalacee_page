92bef162bc3bf0dd2ccb608aa32dbeea38d163ba
Giancarlo <giancarlo.scalisi@dci.education>
2023-09-05 13:45:16 +0200

    slide playbutton und berlinkung

--- src/components/SwiperSlider.jsx
+++ src/components/SwiperSlider.jsx
@@ -18,7 +18,6 @@ import galerie from "../assets/galerie.jpg";
 import poladior from "../assets/poladior.jpg";
 import bombe from "../assets/bombe.jpg";
 import leg_nicht_auf from "../assets/leg_nicht_auf.jpeg";
-import werden_wir_uns_wiedersehen from "../assets/werden_wir_uns_wiedersehen.jpeg";
 
 export default function App() {
   return (
@@ -35,27 +34,71 @@ export default function App() {
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={true}
+        pagination={false} // Punkte darunter deaktiviert
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
           <img src={belalim2023} />
+
+          <h3>
+            Belalim 2023 <br /> Zcalacee & Ziya
+          </h3>
+
+          <a
+            href="https://open.spotify.com/intl-de/track/7D4iG5l7PNrFYnDiB1YXUM"
+            target="_blank"
+          >
+            <button>Play</button>
+          </a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={galerie} />
+          <h3>
+            Galerie <br /> Zcalacee & Momo Chahine
+          </h3>
+          <a
+            href="https://open.spotify.com/intl-de/track/3MO40EQ3yCWS5gSQNLxmDk"
+            target="_blank"
+          >
+            <button>Play</button>
+          </a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={poladior} />
+          <h3>
+            Poladior <br /> Zcalacee
+          </h3>
+          <a
+            href="https://open.spotify.com/intl-de/track/53FoMbvIThwS9FIVEAMm6u"
+            target="_blank"
+          >
+            <button>Play</button>
+          </a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={bombe} />
+          <h3>
+            Bombe <br /> Zcalacee
+          </h3>
+          <a
+            href="https://open.spotify.com/intl-de/album/4J9pSSPIShcelDWy6jpjwM"
+            target="_blank"
+          >
+            <button>Play</button>
+          </a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={leg_nicht_auf} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={werden_wir_uns_wiedersehen} />
+          <h3>
+            Leg nicht auf <br /> Zcalacee
+          </h3>
+          <a
+            href="https://open.spotify.com/intl-de/track/5DHDGIMi6NzNMyjQiAvQ5K"
+            target="_blank"
+          >
+            <button>Play</button>
+          </a>
         </SwiperSlide>
       </Swiper>
     </>
